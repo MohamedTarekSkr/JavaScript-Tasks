966a2b83ecd1bd472df1f4be89819033ee1db5c4
MohamedTarekSkr <117119471+MohamedTarekSkr@users.noreply.github.com>
2022-11-29 10:59:18 +0200

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -22,6 +22,9 @@ const remove = (i) => {
 };
 
 const renderHTML = () => {
+  // update local data
+  localStorage.setItem(`products`, JSON.stringify(products));
+  // end
   document.getElementById("products").innerHTML = "";
   products.forEach((p, i) => {
     document.getElementById("products").innerHTML += getProductHTMLRow(p, i);
@@ -29,37 +32,35 @@ const renderHTML = () => {
   document.getElementById("shipping").innerHTML = `$${getShipping()}`;
   document.getElementById("sub-total").innerHTML = `$${getSubTotal()}`;
   document.getElementById("total").innerHTML = `$${getTotal()}`;
-  // input update data
-  localStorage.setItem(`products`, JSON.stringify(products));
 };
 
 const getProductHTMLRow = (p, i) => {
   return `
   <tr>
-    <td class="align-middle"><img src="img/${
-      p.productName
-    }.jpg" alt="" style="width: 50px;"> ${p.productName}</td>
-    <td class="align-middle">$${p.price}</td>
-    <td class="align-middle">
-        <div class="input-group quantity mx-auto" style="width: 100px;">
-            <div class="input-group-btn">
-                <button type="button" class="btn btn-sm btn-primary btn-minus" onclick="decQuantity(${i})">
-                <i class="fa fa-minus"></i>
-                </button>
-            </div>
-            <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="${
-              p.quantity
-            }">
-            <div class="input-group-btn">
-                <button type="button" class="btn btn-sm btn-primary btn-plus" onclick="incQuantity(${i})">
-                    <i class="fa fa-plus"></i>
-                </button>
-            </div>
-        </div>
-    </td>
-    <td class="align-middle">$${p.price * p.quantity}</td>
-    <td class="align-middle"><button class="btn btn-sm btn-danger" type="button" onclick="remove(${i})"><i class="fa fa-times"></i></button></td>
-</tr>`;
+  <td class="align-middle"><img src="img/${
+    p.productName
+  }.jpg" alt="" style="width: 50px;"> ${p.productName}</td>
+  <td class="align-middle">$${p.price}</td>
+  <td class="align-middle">
+  <div class="input-group quantity mx-auto" style="width: 100px;">
+  <div class="input-group-btn">
+  <button type="button" class="btn btn-sm btn-primary btn-minus" onclick="decQuantity(${i})">
+  <i class="fa fa-minus"></i>
+  </button>
+  </div>
+  <input type="text" class="form-control form-control-sm bg-secondary border-0 text-center" value="${
+    p.quantity
+  }">
+  <div class="input-group-btn">
+  <button type="button" class="btn btn-sm btn-primary btn-plus" onclick="incQuantity(${i})">
+  <i class="fa fa-plus"></i>
+  </button>
+  </div>
+  </div>
+  </td>
+  <td class="align-middle">$${p.price * p.quantity}</td>
+  <td class="align-middle"><button class="btn btn-sm btn-danger" type="button" onclick="remove(${i})"><i class="fa fa-times"></i></button></td>
+  </tr>`;
 };
 
 const products = JSON.parse(localStorage.getItem("products") || "[]");
